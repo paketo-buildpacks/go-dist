0ddb020f1b9621b3989c08e2f6e5a053a2980d89
CF Buildpacks Team CI Server <cf-buildpacks-eng@pivotal.io>
2019-05-10 15:05:57 +0000

    Update CNB tools

--- scripts/package.sh
+++ scripts/package.sh
@@ -9,11 +9,14 @@ PACKAGE_DIR=${PACKAGE_DIR:-"${PWD##*/}_$(openssl rand -hex 4)"}
 full_path=$(realpath "$PACKAGE_DIR")
 args=".bin/packager -uncached"
 
-if [[ $1 == "-c" ]]; then #package as cached
+if [ $1 == "-c" ] || [ $2 == "-c" ]; then #package as cached
     full_path="$full_path-cached"
     args=".bin/packager"
 fi
 
+if [ $1 == "-a" ] || [ $2 == "-a" ]; then #package as archive
+    args="${args} -archive"
+fi
 eval "$args $full_path"
 
 if [[ -n "$BP_REWRITE_HOST" ]]; then
