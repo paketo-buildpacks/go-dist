3c9d8697cd1423e616d94732568d6dcba9ce81de
Ryan Moran <155736+ryanmoran@users.noreply.github.com>
2020-07-02 15:05:32 -0400

    Removes ContainerLogs from integration suite (#74)
    The `BeAvailable` method will already fetch logs if the assertion fails

--- integration/init_test.go
+++ integration/init_test.go
@@ -8,7 +8,6 @@ import (
 	"time"
 
 	"github.com/cloudfoundry/dagger"
-	"github.com/paketo-buildpacks/occam"
 	"github.com/paketo-buildpacks/packit/pexec"
 	"github.com/sclevine/spec"
 	"github.com/sclevine/spec/report"
@@ -55,15 +54,6 @@ func TestIntegration(t *testing.T) {
 	suite("LayerReuse", testLayerReuse)
 	suite("Offline", testOffline)
 	suite.Run(t)
-}
-
-func ContainerLogs(id string) func() string {
-	docker := occam.NewDocker()
-
-	return func() string {
-		logs, _ := docker.Container.Logs.Execute(id)
-		return logs.String()
-	}
 }
 
 func GetGitVersion() (string, error) {
