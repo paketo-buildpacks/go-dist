46b1ba092c14fda971b309d20f24f1988010364e
Ryan Moran <rmoran@vmware.com>
2020-07-02 14:15:12 -0400

    Removes ContainerLogs from integration suite
    The `BeAvailable` method will already fetch logs if the assertion fails

--- integration/buildpack_yaml_test.go
+++ integration/buildpack_yaml_test.go
@@ -66,7 +66,7 @@ func testBuildpackYAML(t *testing.T, context spec.G, it spec.S) {
 			container, err = docker.Container.Run.WithCommand("go run main.go").Execute(image.ID)
 			Expect(err).NotTo(HaveOccurred())
 
-			Eventually(container).Should(BeAvailable(), ContainerLogs(container.ID))
+			Eventually(container).Should(BeAvailable())
 
 			response, err := http.Get(fmt.Sprintf("http://localhost:%s", container.HostPort()))
 			Expect(err).NotTo(HaveOccurred())
