ecde5be5a4cb74cc9afd5b9d2aed01502d3f45cd
Josh Zarrabi <jzarrabi@vmware.com>
2020-09-22 21:03:23 +0000

    explicity add go.mod versions to the  priority list
    resolves paketo-buildpacks/go-mod-vendor/issues/118

--- plan_entry_resolver_test.go
+++ plan_entry_resolver_test.go
@@ -1,6 +1,7 @@
 package godist_test
 
 import (
+	"bytes"
 	"testing"
 
 	godist "github.com/paketo-buildpacks/go-dist"
@@ -14,76 +15,35 @@ func testPlanEntryResolver(t *testing.T, context spec.G, it spec.S) {
 	var (
 		Expect = NewWithT(t).Expect
 
+		buffer   *bytes.Buffer
 		resolver godist.PlanEntryResolver
 	)
 
 	it.Before(func() {
-		resolver = godist.NewPlanEntryResolver()
+		buffer = bytes.NewBuffer(nil)
+		resolver = godist.NewPlanEntryResolver(godist.NewLogEmitter(buffer))
 	})
 
-	context("Resolve", func() {
-		it("chooses a plan entry", func() {
+	context("when a buildpack.yml entry is included", func() {
+		it("resolves the best plan entry", func() {
 			entry := resolver.Resolve([]packit.BuildpackPlanEntry{
 				{
 					Name: "go",
 					Metadata: map[string]interface{}{
-						"number": 1,
+						"version":        "go-mod-version",
+						"version-source": "go.mod",
 					},
 				},
 				{
 					Name: "go",
 					Metadata: map[string]interface{}{
-						"number": 2,
-					},
-				},
-			})
-			Expect(entry).To(Equal(packit.BuildpackPlanEntry{
-				Name: "go",
-				Metadata: map[string]interface{}{
-					"number": 1,
-				},
-			}))
-		})
-
-		it("merges the build and launch flags", func() {
-			entry := resolver.Resolve([]packit.BuildpackPlanEntry{
-				{
-					Name: "go",
-					Metadata: map[string]interface{}{
-						"build":  true,
-						"number": 1,
+						"version": "other-version",
 					},
 				},
 				{
 					Name: "go",
 					Metadata: map[string]interface{}{
-						"launch": true,
-						"number": 2,
-					},
-				},
-			})
-			Expect(entry).To(Equal(packit.BuildpackPlanEntry{
-				Name: "go",
-				Metadata: map[string]interface{}{
-					"build":  true,
-					"launch": true,
-					"number": 1,
-				},
-			}))
-		})
-
-		it("prioritizes versions from buildpack.yml", func() {
-			entry := resolver.Resolve([]packit.BuildpackPlanEntry{
-				{
-					Name: "go",
-					Metadata: map[string]interface{}{
-						"number": 1,
-					},
-				},
-				{
-					Name: "go",
-					Metadata: map[string]interface{}{
-						"number":         2,
+						"version":        "buildpack-yml-version",
 						"version-source": "buildpack.yml",
 					},
 				},
@@ -91,8 +51,95 @@ func testPlanEntryResolver(t *testing.T, context spec.G, it spec.S) {
 			Expect(entry).To(Equal(packit.BuildpackPlanEntry{
 				Name: "go",
 				Metadata: map[string]interface{}{
-					"number":         2,
+					"version":        "buildpack-yml-version",
 					"version-source": "buildpack.yml",
+				},
+			}))
+
+			Expect(buffer.String()).To(ContainSubstring("    Candidate version sources (in priority order):"))
+			Expect(buffer.String()).To(ContainSubstring("      buildpack.yml -> \"buildpack-yml-version\""))
+			Expect(buffer.String()).To(ContainSubstring("      go.mod        -> \"go-mod-version\""))
+			Expect(buffer.String()).To(ContainSubstring("      <unknown>     -> \"other-version\""))
+		})
+	})
+
+	context("when a buildpack.yml entry is not included", func() {
+		it("resolves the best plan entry", func() {
+			entry := resolver.Resolve([]packit.BuildpackPlanEntry{
+				{
+					Name: "go",
+					Metadata: map[string]interface{}{
+						"version":        "go-mod-version",
+						"version-source": "go.mod",
+					},
+				},
+				{
+					Name: "go",
+					Metadata: map[string]interface{}{
+						"version": "other-version",
+					},
+				},
+			})
+			Expect(entry).To(Equal(packit.BuildpackPlanEntry{
+				Name: "go",
+				Metadata: map[string]interface{}{
+					"version":        "go-mod-version",
+					"version-source": "go.mod",
+				},
+			}))
+
+			Expect(buffer.String()).To(ContainSubstring("    Candidate version sources (in priority order):"))
+			Expect(buffer.String()).To(ContainSubstring("      go.mod    -> \"go-mod-version\""))
+			Expect(buffer.String()).To(ContainSubstring("      <unknown> -> \"other-version\""))
+		})
+	})
+
+	context("when entry flags differ", func() {
+		context("OR's them together on best plan entry", func() {
+			it("has all flags", func() {
+				entry := resolver.Resolve([]packit.BuildpackPlanEntry{
+					{
+						Name: "go",
+						Metadata: map[string]interface{}{
+							"version":        "go-mod-version",
+							"version-source": "go.mod",
+						},
+					},
+					{
+						Name: "go",
+						Metadata: map[string]interface{}{
+							"version":        "other-version",
+							"version-source": "other-source",
+							"build":          true,
+						},
+					},
+				})
+				Expect(entry).To(Equal(packit.BuildpackPlanEntry{
+					Name: "go",
+					Metadata: map[string]interface{}{
+						"version":        "go-mod-version",
+						"version-source": "go.mod",
+						"build":          true,
+					},
+				}))
+			})
+		})
+	})
+
+	context("when an unknown source entry is included", func() {
+		it("resolves the best plan entry", func() {
+			entry := resolver.Resolve([]packit.BuildpackPlanEntry{
+				{
+					Name: "go",
+					Metadata: map[string]interface{}{
+						"version": "other-version",
+					},
+				},
+			})
+			Expect(entry).To(Equal(packit.BuildpackPlanEntry{
+				Name: "go",
+				Metadata: map[string]interface{}{
+					"version": "other-version",
 				},
 			}))
 		})
