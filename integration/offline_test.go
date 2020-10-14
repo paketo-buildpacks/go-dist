0bb45b1f2dfe641f2505c7c1b4470907d1235739
Ryan Moran <rmoran@vmware.com>
2020-10-14 10:30:47 -0700

    Uses WithPullPolicy("never") instead of WithNoPull()

--- integration/offline_test.go
+++ integration/offline_test.go
@@ -61,7 +61,7 @@ func testOffline(t *testing.T, context spec.G, it spec.S) {
 			)
 
 			image, logs, err = pack.WithNoColor().Build.
-				WithNoPull().
+				WithPullPolicy("never").
 				WithBuildpacks(offlineBuildpack, buildPlanBuildpack).
 				WithNetwork("none").
 				Execute(name, source)
